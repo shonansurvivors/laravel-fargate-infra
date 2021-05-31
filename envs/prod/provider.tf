475217ed3b7494bab9fe5b369e204c57a068a82b
shonansurvivors <shonansurvivors@gmail.com>
2021-05-31 23:00:48 +0900

    create provider

---
+++ envs/prod/provider.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "ap-northeast-1"
+
+  default_tags {
+    tags = {
+      Env    = "prod"
+      System = "example"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.42.0"
+    }
+  }
+
+  required_version = "0.15.4"
+}
