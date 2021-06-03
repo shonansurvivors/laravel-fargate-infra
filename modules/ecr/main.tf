c2e674185a8aaefc1ecee30c55bb7b135c31cf8a
shonansurvivors <shonansurvivors@gmail.com>
2021-06-03 09:05:57 +0900

    module

---
+++ modules/ecr/main.tf
@@ -0,0 +1,30 @@
+resource "aws_ecr_repository" "this" {
+  name = var.name
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  policy = jsonencode(
+    {
+      "rules": [
+        {
+          "rulePriority": 1,
+          "description": "Keep only ${var.holding_count} images, expire all others",
+          "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": var.holding_count
+          },
+          "action": {
+            "type": "expire"
+          }
+        }
+      ]
+    }
+  )
+
+  repository = aws_ecr_repository.this.name
+}
