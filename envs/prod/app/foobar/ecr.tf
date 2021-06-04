2a2812bcda3d453673b55a0145bfe561b686d583
shonansurvivors <shonansurvivors@gmail.com>
2021-06-05 08:59:04 +0900

    use local values

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -1,11 +1,11 @@
 module "nginx" {
   source = "../../../../modules/ecr"
 
-  name = "example-prod-foobar-nginx"
+  name = "${local.name_prefix}-${local.service_name}-nginx"
 }
 
 module "php" {
   source = "../../../../modules/ecr"
 
-  name = "example-prod-foobar-php"
+  name = "${local.name_prefix}-${local.service_name}-php"
 }
