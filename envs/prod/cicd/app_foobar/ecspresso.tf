7117fef1154103b25a4834bf6813ea6d5352894b
shonansurvivors <shonansurvivors@gmail.com>
2021-06-25 00:58:14 +0900

    use ecspresso

---
+++ envs/prod/cicd/app_foobar/ecspresso.tf
@@ -0,0 +1,58 @@
+// referenced by ecspresso
+
+data "aws_cloudwatch_log_group" "nginx" {
+  name = "/ecs/${local.name_prefix}-${local.service_name}/nginx"
+}
+
+data "aws_cloudwatch_log_group" "php" {
+  name = "/ecs/${local.name_prefix}-${local.service_name}/php"
+}
+
+data "aws_ecr_repository" "nginx" {
+  name = "${local.name_prefix}-${local.service_name}-nginx"
+}
+
+data "aws_ecr_repository" "php" {
+  name = "${local.name_prefix}-${local.service_name}-php"
+}
+
+data "aws_iam_role" "ecs_task_execution" {
+  name = "${local.name_prefix}-${local.service_name}-ecs-task-execution"
+}
+
+data "aws_iam_role" "ecs_task" {
+  name = "${local.name_prefix}-${local.service_name}-ecs-task"
+}
+
+data "aws_lb_target_group" "this" {
+  name = "${local.name_prefix}-${local.service_name}"
+}
+
+data "aws_security_group" "vpc" {
+  name = "${local.name_prefix}-main-vpc"
+}
+
+data "aws_subnet" "private" {
+  for_each = var.azs
+
+  tags = {
+    Name = "${local.name_prefix}-main-private-${each.key}"
+  }
+}
+
+variable "azs" {
+  type = map(object({
+    public_cidr  = string
+    private_cidr = string
+  }))
+  default = {
+    a = {
+      public_cidr  = "172.32.0.0/20"
+      private_cidr = "172.32.48.0/20"
+    },
+    c = {
+      public_cidr  = "172.32.16.0/20"
+      private_cidr = "172.32.64.0/20"
+    }
+  }
+}
--- envs/prod/cicd/app_foobar/iam.tf
+++ envs/prod/cicd/app_foobar/iam.tf
@@ -11,16 +11,16 @@ resource "aws_iam_role" "deployer" {
 
   assume_role_policy = jsonencode(
     {
-      "Version": "2012-10-17",
-      "Statement": [
+      "Version" : "2012-10-17",
+      "Statement" : [
         {
-          "Effect": "Allow",
-          "Action": [
+          "Effect" : "Allow",
+          "Action" : [
             "sts:AssumeRole",
             "sts:TagSession"
           ],
-          "Principal": {
-            "AWS": aws_iam_user.github.arn
+          "Principal" : {
+            "AWS" : aws_iam_user.github.arn
           }
         }
       ]
@@ -39,4 +39,67 @@ data "aws_iam_policy" "ecr_power_user" {
 resource "aws_iam_role_policy_attachment" "role_deployer_policy_ecr_power_user" {
   role       = aws_iam_role.deployer.name
   policy_arn = data.aws_iam_policy.ecr_power_user.arn
+}
+
+resource "aws_iam_role_policy" "s3" {
+  name = "s3"
+  role = aws_iam_role.deployer.id
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:GetObject"
+          ],
+          "Resource" : "arn:aws:s3:::shonansurvivors-tfstate/${local.system_name}/${local.env_name}/cicd/app_${local.service_name}_*.tfstate"
+        },
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy" "ecs" {
+  name = "ecs"
+  role = aws_iam_role.deployer.id
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "RegisterTaskDefinition",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecs:RegisterTaskDefinition"
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Sid" : "PassRolesInTaskDefinition",
+          "Effect" : "Allow",
+          "Action" : [
+            "iam:PassRole"
+          ],
+          "Resource" : [
+            data.aws_iam_role.ecs_task.arn,
+            data.aws_iam_role.ecs_task_execution.arn,
+          ]
+        },
+        {
+          "Sid" : "DeployService",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecs:UpdateService",
+            "ecs:DescribeServices"
+          ],
+          "Resource" : [
+            data.aws_ecs_service.this.arn
+          ]
+        }
+      ]
+    }
+  )
 }
